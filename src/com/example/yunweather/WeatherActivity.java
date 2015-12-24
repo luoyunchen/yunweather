2c08da41e19a6641a7c41c82e61c1d89ad4a3f8c
LuoYun <565013853@qq.com>
2015-12-24 14:43:14 +0800

    增加后台自动更新天气的功能。

--- src/com/example/yunweather/WeatherActivity.java
+++ src/com/example/yunweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.yunweather;
 
+import service.AutoUpdateService;
 import util.HttpCallbackListener;
 import util.HttpUtil;
 import util.Utility;
@@ -176,6 +177,8 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 	
 }
